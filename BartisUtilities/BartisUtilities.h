76406b76c0d97ca0b3fa2f9b69f0d5a72bbbb36f
Alex Bartis <alex.bartis@gmail.com>
2017-05-05 20:53:02 +0300

    Initial Commit

---
+++ BartisUtilities/BartisUtilities.h
@@ -0,0 +1,19 @@
+//
+//  BartisUtilities.h
+//  BartisUtilities
+//
+//  Created by Alex Bartis on 05/05/2017.
+//  Copyright © 2017 Alex Bartis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BartisUtilities.
+FOUNDATION_EXPORT double BartisUtilitiesVersionNumber;
+
+//! Project version string for BartisUtilities.
+FOUNDATION_EXPORT const unsigned char BartisUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BartisUtilities/PublicHeader.h>
+
+
